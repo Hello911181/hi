c1e6b52efdb6dd8c4fdf6ec34b9ca10a3443139f
Hello911181 <94292098+Hello911181@users.noreply.github.com>
2021-11-14 17:36:50 +0600

    update

--- cc.py
+++ cc.py
@@ -1,1 +1,33 @@
-Hi
+#Here is a complete snippet:
+import base64
+import requests 
+
+import json
+def push_to_github(filename, repo, branch, token):
+    url="https://api.github.com/repos/"+repo+"/contents/"+filename
+
+    base64content=base64.b64encode(open(filename,"rb").read())
+
+    data = requests.get(url+'?ref='+branch, headers = {"Authorization": "token "+token}).json()
+    sha = data['sha']
+
+    if base64content.decode('utf-8')+"\n" != data['content']:
+        message = json.dumps({"message":"update",
+                            "branch": branch,
+                            "content": base64content.decode("utf-8") ,
+                            "sha": sha
+                            })
+
+        resp=requests.put(url, data = message, headers = {"Content-Type": "application/json", "Authorization": "token "+token})
+
+        print(resp)
+    else:
+        print("nothing to update")
+
+token = "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"
+
+filename="cc.py"
+repo = "hello911181/hi"
+branch="main"
+
+push_to_github(filename, repo, branch, token)
